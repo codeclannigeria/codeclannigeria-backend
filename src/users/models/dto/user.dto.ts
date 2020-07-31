53b027aa44365a559dc83b312a79fdc2ea69a9c0
Fortune <fortuneochi@gmail.com>
2020-07-31 13:34:12 +0100

    feat: added more properties to user update. closes #93

--- src/users/models/dto/user.dto.ts
+++ src/users/models/dto/user.dto.ts
@@ -1,11 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Exclude, Expose, Type } from 'class-transformer';
-import { IsArray, IsEmail, IsEnum, IsOptional, IsPhoneNumber, Length, MaxLength } from 'class-validator';
+import { IsArray, IsEmail, IsEnum, IsOptional, IsPhoneNumber, Length, MaxLength, IsDate } from 'class-validator';
 import { columnSize } from '~shared/constants';
 import { BaseDto, PagedListDto } from '~shared/models/dto';
 
 import { TrackDto } from '../../../tracks/models/dto/track.dto';
-import { UserRole } from '../user.entity';
+import { UserRole, Gender } from '../user.entity';
 
 @Exclude()
 export class UserDto extends BaseDto {
@@ -31,6 +31,25 @@ export class UserDto extends BaseDto {
   @IsOptional()
   @MaxLength(columnSize.length128)
   description?: string;
+
+  @Expose()
+  @MaxLength(columnSize.length64)
+  city: string;
+
+  @Expose()
+  @MaxLength(columnSize.length64)
+  country: string;
+
+  @Expose()
+  @IsEnum(Gender)
+  @ApiProperty({ enum: Gender })
+  gender: Gender;
+
+  @Expose()
+  @IsDate()
+  @ApiProperty({ type: Date })
+  @Type(() => Date)
+  dob: Date;
 
   @Expose()
   @IsPhoneNumber("ZZ", { message: 'Invalid phone number. Valid phone number sample +2347063644568' })
