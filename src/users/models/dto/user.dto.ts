33502a7af6bf30a51218f0ad6088b445a203be0d
Fortune <fortuneochi@gmail.com>
2020-09-23 23:49:58 +0100

    feat: added more props to user

--- src/users/models/dto/user.dto.ts
+++ src/users/models/dto/user.dto.ts
@@ -79,6 +79,16 @@ export class UserDto extends BaseDto {
   @IsOptional()
   readonly photoUrl?: string;
 
+  @ApiProperty({ readOnly: true })
+  @Expose()
+  @IsOptional()
+  readonly notifyCount: number;
+
+  @ApiProperty({ readOnly: true })
+  @Expose()
+  @IsOptional()
+  readonly unreadCount: number;
+
   @ApiProperty({ readOnly: true, type: [TrackDto] })
   @Expose()
   @Type(() => TrackDto)
