9077c40c669641d32bbebe49cbfd311e386a214b
Fortune <fortuneochi@gmail.com>
2020-10-07 23:48:55 +0100

    fix: display tracks of a user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,17 @@
 import {
   Body,
   ConflictException,
+  Get,
   HttpStatus,
   Post,
+  Query,
   UseGuards
 } from '@nestjs/common';
 import { ApiBearerAuth, ApiResponse } from '@nestjs/swagger';
+import { plainToClass } from 'class-transformer';
 import { Roles } from '~shared/decorators/roles.decorator';
 import { ApiException } from '~shared/errors';
+import { FindDto, IPagedListDto } from '~shared/models/dto';
 
 import { JwtAuthGuard, RolesGuard } from '../auth/guards';
 import { BaseCrudController } from '../shared/controllers/base.controller';
@@ -51,5 +55,44 @@ export class UsersController extends BaseCtrl {
       );
     }
     return super.create(input);
+  }
+  @Get()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN)
+  @ApiResponse({ type: PagedUserOutputDto, status: HttpStatus.OK })
+  @ApiBearerAuth()
+  async findAll(@Query() query: FindDto): Promise<IPagedListDto<UserDto>> {
+    const { skip, limit, search, opts } = query;
+    const { conditions, options } = this.getConditions(search, opts);
+    const entities = await this.usersService
+      .findAll(conditions, options)
+      .populate('tracks')
+      .limit(limit)
+      .skip(skip)
+      .lean()
+      .exec();
+
+    const totalCount = await this.usersService.countAsync();
+    const items = plainToClass(UserDto, entities, {
+      enableImplicitConversion: true,
+      excludeExtraneousValues: true
+    }) as any;
+    return { totalCount, items };
+  }
+
+  private getConditions(search: string, opts: string) {
+    const conditions = JSON.parse(search || '{}');
+    const options = JSON.parse(opts || '{}');
+
+    if (options?.sort) {
+      const sort = { ...options.sort };
+      Object.keys(sort).map((key) => {
+        if (sort[key] === 'ascend') sort[key] = 1;
+        else sort[key] = -1;
+        return sort;
+      });
+      options.sort = sort;
+    }
+    return { conditions, options };
   }
 }
